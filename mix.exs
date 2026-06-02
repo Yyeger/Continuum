c05a50ef8e1fa90cdc711f06ff69c1e6c4412dd6
yyeger <idjersamyremy@gmail.com>
2026-06-02 17:46:52 +0200

    fix(mix): fix source

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Continuum.MixProject do
   use Mix.Project
 
   @version "0.5.0"
-  @source_url "https://github.com/continuum-elixir/continuum"
+  @source_url "https://github.com/Yyeger/Continuum"
 
   def project do
     [
